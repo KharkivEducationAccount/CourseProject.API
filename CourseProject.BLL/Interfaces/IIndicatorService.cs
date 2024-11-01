00501ab1f8216185bb566c986a90459be38f7531
KharkivEducationAccount <oleh.yevseienko@khpi.edu.ua>
2024-11-01 22:21:06 +0200

    Add project files.

---
+++ CourseProject.BLL/Interfaces/IIndicatorService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject.BLL.Interfaces
+{
+    public interface IIndicatorService
+    {
+        Task CreateIndicator(Indicator model);
+        Task<List<Indicator>> GetIndicators();
+        Task DeleteIndicator(Guid id);
+        Task UpdateIndicatorValue(Indicator model);
+
+    }
+}
