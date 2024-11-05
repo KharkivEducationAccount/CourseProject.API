eb0e49eb27967a90266c525c227affe9a14ac3a4
KharkivEducationAccount <oleh.yevseienko@khpi.edu.ua>
2024-11-05 22:31:47 +0200

    Registered services and added signalR connection.

--- CourseProject.API/Program.cs
+++ CourseProject.API/Program.cs
@@ -1,15 +1,34 @@
-var builder = WebApplication.CreateBuilder(args);
+using CourseProject.BLL.Interfaces;
+using CourseProject.BLL.Services;
+using CourseProject.Core.Constants;
+using CourseProject.Core.Options;
+using CourseProject.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.Configure<DbOptions>(
+    builder.Configuration.GetSection(
+        OptionsConstants.DbOptionsKey));
+
+builder.Services.AddDbContext<CourseProjectDbContext>((provider, ctx) =>
+{
+    var options = provider.GetRequiredService<IOptions<DbOptions>>().Value;
+    ctx.UseSqlServer(options.ConnectionString);
+});
+
+builder.Services.AddScoped<IIndicatorService, IndicatorService>();
+
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
